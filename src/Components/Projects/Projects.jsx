291960fea1ff222c087a6286f3ac3356dbc85e04
Maha Marwa Bourada <mahamarwabourada@gmail.com>
2025-06-21 12:31:32 +0100

    Project rearrangement

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -7,45 +7,45 @@ const Projects = () => {
   const { t } = useTranslation();
   const projects = projectsData;
 
+  console.log(projects)
+
   return (
-    <div
-      className="mx-8 md:mx-16 pt-10 lg:pt-[4.5rem] lg:ml-24 my-20"
-    >
+    <div className="mx-8 md:mx-16 pt-10 lg:pt-[4.5rem] lg:ml-24 my-20">
       {/* ml-32 mt-32 pt-32 */}
       <h1 className="mb-7 font-main text-2xl text-hippie-blue-400">
         {/*  mb-10 */}
         {t("Projects.title")}
       </h1>
-      
-        {projects.map((project) => {
-          return (
-            <>
-              {project.id % 2 === 0 ? (
-                <RightProjectCard
-                  projectID={project.id}
-                  title={t(`Projects.list.${project.id}.title`)}
-                  lang={project.lang}
-                  subBool={project.subDesc}
-                  subDesc={t(`Projects.list.${project.id}.subDesc`)}
-                  desc={t(`Projects.list.${project.id}.desc`)}
-                  image={project.image}
-                  url={project.url}
-                />
-              ) : (
-                <LeftProjectCard
-                  projectID={project.id}
-                  title={t(`Projects.list.${project.id}.title`)}
-                  lang={project.lang}
-                  subBool={project.subDesc}
-                  subDesc={t(`Projects.list.${project.id}.subDesc`)}
-                  desc={t(`Projects.list.${project.id}.desc`)}
-                  image={project.image}
-                  url={project.url}
-                />
-              )}
-            </>
-          );
-        })}
+
+      {projects.map((project) => {
+        return (
+          <>
+            {project.id % 2 === 0 ? (
+              <RightProjectCard
+                projectID={project.id}
+                title={t(`Projects.list.${project.id}.title`)}
+                lang={project.lang}
+                subBool={project.subDesc}
+                subDesc={t(`Projects.list.${project.id}.subDesc`)}
+                desc={t(`Projects.list.${project.id}.desc`)}
+                image={project.image}
+                url={project.url}
+              />
+            ) : (
+              <LeftProjectCard
+                projectID={project.id}
+                title={t(`Projects.list.${project.id}.title`)}
+                lang={project.lang}
+                subBool={project.subDesc}
+                subDesc={t(`Projects.list.${project.id}.subDesc`)}
+                desc={t(`Projects.list.${project.id}.desc`)}
+                image={project.image}
+                url={project.url}
+              />
+            )}
+          </>
+        );
+      })}
     </div>
   );
 };
